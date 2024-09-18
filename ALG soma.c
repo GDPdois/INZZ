b6b73a6927e6839a3faac17dc12536421fada9fa
T4lesbyte <thalescpcontato@gmail.com>
2024-09-18 19:43:41 -0300

    Create ALG soma.c

---
+++ ALG soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+Int main ()
+     float p1, p2, total;
+
+printf ("\nInsira o valor do produto");
+scanf ("%f", &p1);
+printf ("\nInsira o valor do outro produto");
+scanf ("%f", &p2)
+
+  total = p1 + p2;
+
+printf ("\nTotal a pagar: %f", total);
+
+return 0;
