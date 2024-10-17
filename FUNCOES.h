c92f7edaa17b1e1d000c64da8a9810bb110eaa3f
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-10-17 10:16:17 -0300

    Update FUNCOES.h

--- FUNCOES.h
+++ FUNCOES.h
@@ -1,11 +1,41 @@
+
 //ARQUIVO DE FUNÇÕES
+
+//SOMA PRODUTOS
+
+float somaProdutos(){
+    float preco[100], total=0;
+    char controle='S';
+    int i=0;
+    do{
+
+    printf("\nO valor total da compra está em %.2f \n", total);
+    printf("Digite o valor do %dº produto \n", i+1);
+    scanf("%f", &preco[i]);
+    total += preco[i];
+    i++;
+
+
+    printf("Quer mais? Caso sim digite S \n");
+    fflush(stdin);
+    scanf("%c", &controle);
+    controle=tolower(controle);
+    system("cls");
+
+    }
+    while(controle == 's');
+
+    printf("O valor da compra foi %.2f", total);
+
+    return total;
+}
 
 //MÓDULO CAIXA DESCONTO
 
-void moduloCaixaDesconto(){
-    float valorCompra, descontoPercentual, valorFinal;
+void moduloCaixaDesconto(float totalSemDesc){
+    float /*valorCompra,*/ descontoPercentual, valorFinal;
 
-    do{
+    /*do{
         printf("\nDigite o valor da compra: R$ ");
         scanf("%f", &valorCompra);
 
@@ -16,10 +46,10 @@ void moduloCaixaDesconto(){
     }
     while(valorCompra <= 0);
 
-    system("cls");
+    system("cls");*/
 
     do{
-        printf("\nValor da compra: R$ %.2f\n", valorCompra);
+        printf("\nValor da compra: R$ %.2f\n", totalSemDesc);
         printf("\nDigite a porcentagem do desconto: ");
         scanf("%f", &descontoPercentual);
 
@@ -30,11 +60,11 @@ void moduloCaixaDesconto(){
     }
     while(descontoPercentual <= 0 || descontoPercentual > 100);
 
-    valorFinal = valorCompra - (valorCompra * descontoPercentual / 100);
+    valorFinal = totalSemDesc - (totalSemDesc * descontoPercentual / 100);
 
     system("cls");
 
-    printf("\nValor da compra: R$ %.2f\n", valorCompra);
+    printf("\nValor da compra: R$ %.2f\n", totalSemDesc);
     printf("\nDesconto: %.f%%\n", descontoPercentual);
     printf("\nValor final: R$ %.2f\n", valorFinal);
 }
