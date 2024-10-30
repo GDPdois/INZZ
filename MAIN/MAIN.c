0eefccf90f118c8e9e055e626877e7b9de65bda5
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2024-10-30 17:47:30 -0300

    Update MAIN.c

--- MAIN/MAIN.c
+++ MAIN/MAIN.c
@@ -11,45 +11,11 @@
 int main(){
     setlocale(LC_ALL, "");
 
-    char op = '1';
-
     tela_boas_vindas();
     Sleep(2000);
     limpaTela();
 
-    do{
-        printf("\n\n\t - Menu Principal -\n\n");
-        printf("  1. Registrar\n");
-        printf("  2. Login\n");
-        printf("  3. Sair\n");
-        printf("\n  - Escolha: ");
-        scanf(" %c", &op);
-
-        switch(op){
-            case '1':
-                limpaTela();
-                registrar_usuario();
-            break;
-
-            case '2':
-                limpaTela();
-                login_usuario();
-            break;
-
-            case '3':
-                limpaTela();
-                printf("\n\nPrograma encerrado\n\n\n");
-                exit(0);
-            break;
-
-            default:
-                printf("Opção inválida! Aperte qualquer tecla para tentar novamente");
-                getch();
-                limpaTela();
-            break;
-        }
-    }
-    while(op < '1' || op > '3');
+    menuPrincipal();
 
     return 0;
 }
